81926fbabb8ce9d873e522590770317ae7ef2ae9
Xad <xadnarr@gmail.com>
2025-01-16 23:18:32 +0100

    a

--- Assets/MENU/SetDefaultResolution.cs
+++ Assets/MENU/SetDefaultResolution.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SetDefaultResolution : MonoBehaviour
 {
     void Start()
     {
+        SceneManager.LoadScene(0);
+        // Verificar si ya se ha guardado el índice de resolución
         if (!PlayerPrefs.HasKey("ResolutionIndex"))
         {
-            PlayerPrefs.SetInt("ResolutionIndex", 0); // Guardar índice de resolución predeterminada
-            Screen.SetResolution(1920, 1080, true);  // Cambiar a true para pantalla completa
+            // Guardar el índice de resolución predeterminado
+            PlayerPrefs.SetInt("ResolutionIndex", 0);
+            // Cambiar a resolución 1920x1080 en modo pantalla completa
+            Screen.SetResolution(1920, 1080, true);
+            // Guardar los cambios
             PlayerPrefs.Save();
             Debug.Log("Resolución predeterminada guardada: 1920x1080");
         }
